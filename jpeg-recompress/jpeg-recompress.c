269d1fe626569550e06b6fb3547b8b65ddcc3764
Cyril Margorin <cyril@imperx.com>
2019-10-04 19:05:43 +0300

    switch to libjpeg-turbo (from mozjpeg)

--- jpeg-recompress/jpeg-recompress.c
+++ jpeg-recompress/jpeg-recompress.c
@@ -22,7 +22,7 @@ static const char *COMMENT = "Compressed by jpeg-recompress";
 double jr_get_default_target(enum jpeg_recompress_quality_t quality, enum image_compare_method_t method)
 {
     static const double default_targets[4][4] = {
-    /*   LOW     MEDIUM  HIGH     VERYHIGH */
+        /*   LOW     MEDIUM  HIGH     VERYHIGH */
         {0.999,  0.9999, 0.99995, 0.99999}, /* SSIM */
         {0.85,   0.94,   0.96,    0.98}, /* MS-SSIM */
         {100.75, 102.25, 103.8,   105.5}, /* SMALLFRY */
@@ -34,7 +34,7 @@ double jr_get_default_target(enum jpeg_recompress_quality_t quality, enum image_
 
 
 static size_t decompress_jpeg(struct jpeg_decompress_struct *dinfo, const void *in_jpeg, size_t in_jpeg_size,
-                            J_COLOR_SPACE out_color_space, unsigned char *output_image)
+                              J_COLOR_SPACE out_color_space, unsigned char *output_image)
 {
     JSAMPARRAY buffer;
 
@@ -84,7 +84,7 @@ void destroy_marker_list(jpeg_saved_marker_ptr marker_list)
 }
 
 int jpeg_recompress(const void *in_jpeg, size_t in_jpeg_size, const struct jpeg_recompress_config_t *config,
-                             unsigned char **out_jpeg, size_t *out_jpeg_size)
+                    unsigned char **out_jpeg, size_t *out_jpeg_size)
 {
     unsigned int quality = 0;
 
@@ -159,11 +159,11 @@ int jpeg_recompress(const void *in_jpeg, size_t in_jpeg_size, const struct jpeg_
 
 
     for(unsigned int attempt = 0; attempt<config->attempts; ++attempt) {
-	if(compressed) {
-	  free(compressed);
-	  compressed=NULL;
-	  compressed_size = 0;
-	}
+        if(compressed) {
+            free(compressed);
+            compressed=NULL;
+            compressed_size = 0;
+        }
         jpeg_mem_dest(&cinfo, &compressed, &compressed_size);
         double metric;
         quality = (jpeg_min + jpeg_max)/2;
@@ -171,37 +171,14 @@ int jpeg_recompress(const void *in_jpeg, size_t in_jpeg_size, const struct jpeg_
         if(jpeg_min == jpeg_max)
             last_attempt = true;
         bool progressive = last_attempt?!config->no_progressive:false;
-        bool optimize = (config->accurate || last_attempt);
         cinfo.image_width = dinfo.output_width;
         cinfo.image_height = dinfo.output_height;
         cinfo.input_components = 3;
         cinfo.in_color_space = JCS_RGB;
-        if(!optimize) {
-            if (jpeg_c_int_param_supported(&cinfo, JINT_COMPRESS_PROFILE)) {
-                jpeg_c_set_int_param(&cinfo, JINT_COMPRESS_PROFILE, JCP_FASTEST);
-            }
-        }
+
         jpeg_set_defaults(&cinfo);
 
-        if(!optimize) {
-            if (jpeg_c_bool_param_supported(&cinfo, JBOOLEAN_TRELLIS_QUANT)) {
-                jpeg_c_set_bool_param(&cinfo, JBOOLEAN_TRELLIS_QUANT, FALSE);
-            }
-            if (jpeg_c_bool_param_supported(&cinfo, JBOOLEAN_TRELLIS_QUANT_DC)) {
-                jpeg_c_set_bool_param(&cinfo, JBOOLEAN_TRELLIS_QUANT_DC, FALSE);
-            }
-        }
-
-        if (optimize && !progressive) {
-            // Moz defaults, disable progressive
-            cinfo.scan_info = NULL;
-            cinfo.num_scans = 0;
-            if (jpeg_c_bool_param_supported(&cinfo, JBOOLEAN_OPTIMIZE_SCANS)) {
-                jpeg_c_set_bool_param(&cinfo, JBOOLEAN_OPTIMIZE_SCANS, FALSE);
-            }
-        }
-
-        if (!optimize && progressive) {
+        if (progressive) {
             // No moz defaults, set scan progression
             jpeg_simple_progression(&cinfo);
         }
